0ae862d4e297be4d46d64f0ea6bc9a2cd145aafe
fenergy <mustafafenercieem@gmail.com>
2023-05-17 13:35:53 +0300

    V1.0

--- main.pas
+++ main.pas
@@ -1,13 +1,14 @@
-unit main;
+﻿unit main;
 
 interface
 
 uses
   System.SysUtils, System.Types, System.UITypes, System.Classes,
+  IdGlobal,
   System.Variants,
   FMX.Types, FMX.Controls, FMX.Forms, FMX.Graphics, FMX.Dialogs, FMX.Menus,
   FMX.StdCtrls, FMX.Controls.Presentation, FMX.Edit, FMX.Objects,
-  FMX.Memo.Types, FMX.ScrollBox, FMX.Memo;
+  FMX.Memo.Types, FMX.ScrollBox, FMX.Memo, XXTEA, Base64;
 
 type
   TFormMain = class(TForm)
@@ -44,9 +45,12 @@ type
     encrption_key_copy: TButton;
     decryption_text_copy: TButton;
     decryption_key_copy: TButton;
-    procedure encryption_encryptedClick(Sender: TObject);
+    procedure encryption_clearClick(Sender: TObject);
+    procedure encryptClick(Sender: TObject);
+    procedure decryptClick(Sender: TObject);
+    procedure encryption_text_copyClick(Sender: TObject);
+    procedure encrption_key_copyClick(Sender: TObject);
     procedure encryption_encrypted_copyClick(Sender: TObject);
-    procedure encrption_keyClick(Sender: TObject);
   private
     { Private declarations }
   public
@@ -56,28 +60,61 @@ type
 var
   FormMain: TFormMain;
 
+const
+
+  xtea_local_send_key = 'NURMustafa34063';
+  xtea_local_receive_key = 'NURMustafa34064';
+
 implementation
 
 {$R *.fmx}
 
-uses
-  FMX.Clipboard;
+
+procedure TFormMain.encryption_text_copyClick(Sender: TObject);
+begin
+
+  // NURMustafa27385
+  encryption_text.SelectAll;
+  encryption_text.CopyToClipboard();
+end;
+
+procedure TFormMain.decryptClick(Sender: TObject);
+var
+  S, K, ED, DD: TBytes;
+begin
+  DD := Base64.Decode(decryption_text.Text);
+  K := BytesOf(decryption_key.Text);
+  DD := XXTEA.decrypt(DD, K);
+  decryption_decrypted.Text := TEncoding.UTF8.GetString(DD);
+end;
+
+procedure TFormMain.encrption_key_copyClick(Sender: TObject);
+begin
+  encrption_key.SelectAll;
+  encrption_key.CopyToClipboard();
+end;
+
+procedure TFormMain.encryptClick(Sender: TObject);
+var
+  S, K, ED, DD: TBytes;
+begin
+   S := BytesOf(UTF8String(encryption_text.Text));
+   K := BytesOf(encrption_key.Text);
+   ED := XXTEA.Encrypt(S, K);
+   encryption_encrypted.Text := Base64.Encode(ED);
+end;
+
+procedure TFormMain.encryption_clearClick(Sender: TObject);
+begin
+  encrption_key.Text := '';
+  encryption_text.Text := '';
+  encryption_encrypted.Text := '';
+end;
 
 procedure TFormMain.encryption_encrypted_copyClick(Sender: TObject);
 begin
   encryption_encrypted.SelectAll;
   encryption_encrypted.CopyToClipboard();
-end;
-
-procedure TFormMain.encrption_keyClick(Sender: TObject);
-begin
-  encrption_key.SelectAll;
-end;
-
-procedure TFormMain.encryption_encryptedClick(Sender: TObject);
-begin
-  encryption_encrypted.SelectAll;
-
 end;
 
 end.
